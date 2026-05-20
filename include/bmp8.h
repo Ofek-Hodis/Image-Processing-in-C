a1039d03dcbeb059793e39e7783fb7230b68e677
Alexis Pacitto <alexispacitto94@gmail.com>
2026-05-20 19:16:39 +0200

    small change in bmp8.h

--- include/bmp8.h
+++ include/bmp8.h
@@ -10,7 +10,7 @@ typedef struct {
     unsigned int dataSize;
 } t_bmp8;
 
-void bmp8_loadImage(const char * filename);
+t_bmp8 * bmp8_loadImage(const char * filename);
 void bmp8_saveImage(const char * filename, t_bmp8 * img);
 void bmp8_free(t_bmp8 * img);
 void bmp8_printInfo(t_bmp8 * img);
