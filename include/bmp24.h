dac39556862a0eb70e106e4c075b394395f16bb1
Alexis <alexispacitto94@gmail.com>
2026-06-11 10:23:41 +0200

    alloc func in bmp24

--- include/bmp24.h
+++ include/bmp24.h
@@ -3,6 +3,23 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// Constantes pour les offsets des champs de l'en-tête BMP
+#define BITMAP_MAGIC 0x00 // offset 0
+#define BITMAP_SIZE 0x02 // offset 2
+#define BITMAP_OFFSET 0x0A // offset 10
+#define BITMAP_WIDTH 0x12 // offset 18
+#define BITMAP_HEIGHT 0x16 // offset 22
+#define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+// Constante pour le type de fichier BMP
+#define BMP_TYPE 0x4D42 // 'BM' en hexadécimal
+// Constantes pour les tailles des champs de l'en-tête BMP
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+// Constantes pour les valeurs de profondeur de couleur
+#define DEFAULT_DEPTH 0x18 // 24
 
 typedef struct {
 uint16_t type;
--- src/bmp24.c
+++ src/bmp24.c
@@ -1,2 +1,70 @@
 #include "bmp24.h"
 
+t_pixel ** bmp24_allocateDataPixels (int width, int height){
+
+    t_pixel ** pixel_mat = malloc(height*sizeof(t_pixel*));
+
+    if (pixel_mat == NULL){
+        printf("unexpected malloc error in bmp24_allocateDataPixels\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < height; i++){
+        pixel_mat[i] = malloc(width*sizeof(t_pixel));
+
+        if (pixel_mat[i] == NULL){
+            printf("unexpected malloc error in bmp24_allocateDataPixels\n");
+            bmp24_freeDataPixels(pixel_mat, i - 1);
+            return NULL;
+        }
+    }
+
+    return pixel_mat;
+}
+
+
+void bmp24_freeDataPixels (t_pixel ** pixel_mat, int height){
+
+    for (int i = 0; i < height; i ++){
+        free(pixel_mat[i]);
+    }
+
+    free(pixel_mat);
+
+}
+
+t_bmp24 * bmp24_allocate (int width, int height, int colorDepth){
+    
+    t_pixel ** pixel_mat = bmp24_allocateDataPixels(width, height);
+    if (pixel_mat == NULL) return NULL;
+
+    t_bmp24 * bmp24 = malloc(sizeof(t_bmp24));
+    if (bmp24 == NULL){
+        printf("unexpected malloc error in bmp24_allocate\n");
+        bmp24_freeDataPixels(pixel_mat, height);
+    }
+    
+    bmp24->data = pixel_mat;
+    bmp24->width = width;
+    bmp24->height = height;
+    bmp24->colorDepth = colorDepth;
+
+    return bmp24;
+
+}
+
+void bmp24_free (t_bmp24 * img){
+
+    bmp24_freeDataPixels(img->data, img->height);
+    free(img);
+}
+
+void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+    fseek(file, position, SEEK_SET);
+    fread(buffer, size, n, file);
+}
+
+void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+    fseek(file, position, SEEK_SET);
+    fwrite(buffer, size, n, file);
+}
