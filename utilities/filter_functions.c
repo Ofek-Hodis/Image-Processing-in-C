9a62f0912cf920b9fc4ee16c102246e3d8f20fd9
Ofek-Hodis <ofkhod@gmail.com>
2026-06-18 01:24:10 +0200

    Added functions used for filter handling and fixed an error in utilities.h

--- utilities/filter_functions.c
+++ utilities/filter_functions.c
@@ -1,3 +1,52 @@
-//
-// Created by ofkho on 16/06/2026.
-//
+#include "filter_functions.h"
+
+void filter_handling(t_bmp8* img_8, t_bmp24* img_24, float** kernel, int kernelSize, int img_depth)
+{
+    if (img_depth == 8 )
+    {
+        bmp8_applyFilter(img_8, kernel, kernelSize);
+        return;
+    }
+    if (img_depth == 24)
+    {
+        bmp24_convolution(img_24, kernel, kernelSize);
+        return;
+    }
+    printf("Color depth not accepted (expected 8 or 24) - verify file type\n");
+}
+
+void free_kernel(float** kernel, int kernelSize)
+{
+    if (kernel == NULL)
+    {
+        printf("Kernell already empty");
+        return;
+    }
+    for (int i=0; i<kernelSize; i++) // Creation of blur kernel
+    {
+        if (kernel[i] != NULL) free(kernel[i]);
+    }
+    free(kernel);
+}
+
+int allocate_kernel(float*** kernel, int kernelSize)
+{
+    //calloc necessary in case the second malloc fails, so all of the lignes allocated can be freed
+    *kernel = calloc(kernelSize, sizeof(float*));
+    if (*kernel == NULL)
+    {
+        printf("Memory allocation failed");
+        return 0;
+    }
+    for (int i=0; i<kernelSize; i++)
+    {
+        (*kernel)[i] = malloc(kernelSize*sizeof(float));
+        if ((*kernel)[i] == NULL)
+        {
+            printf("Memory allocation failed");
+            free_kernel(*kernel, kernelSize);
+            return 0;
+        }
+    }
+    return 1;
+}
--- utilities/utilities.h
+++ utilities/utilities.h
@@ -9,10 +9,8 @@
 
 float is_in_8bit_range(float pixel_color);
 int compare_size(const size_t size1, const size_t size2, const char* error_message);
-
-int compare_size(size_t size1, size_t size2, const char* error_message);
 int value_limit(int new_val);
 void set_pixel_value(t_bmp8 * img, int i, int new_val);
-
+int find_colorDepth(const char* filename);
 
 #endif //UTILITIES_H
